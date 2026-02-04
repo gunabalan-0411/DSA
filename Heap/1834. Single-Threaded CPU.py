00ff61713912aae77f3ddfe3fb070862b86559ae
Gunabalan <69943544+gunabalan-0411@users.noreply.github.com>
2026-02-04 04:20:40 +0000

    update

---
+++ Heap/1834. Single-Threaded CPU.py
@@ -0,0 +1,27 @@
+from typing import List
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        # Attaching the index before sorting it
+        for i, task in enumerate(tasks):
+            task.append(i)
+        tasks.sort(key=lambda t: t[0])
+
+        res, minHeap = [], []
+        i, time = 0, 0
+
+        while minHeap or i < len(tasks):
+            # adding tasks to minHeap
+            while i < len(tasks) and time >= tasks[i][0]:
+                # adding processing time, its index
+                heapq.heappush(minHeap, (tasks[i][1], tasks[i][2]) )
+                i += 1
+            # if there is no tasks, fastforward time to current process arrival time
+            if not minHeap:
+                time = tasks[i][0]
+            else:
+                processing_time, index = heapq.heappop(minHeap)
+                time += processing_time
+                res.append(index)
+        return res
+
+
